7d38578fb888be15bc23c236ada413bb0b3c875b
Sakshi kalyan jadhav <sakshikalyanjadhav@Sakshis-MacBook-Air.local>
2024-07-17 19:13:28 +0530

    redirect-uri-change

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import session from "express-session";
+// import passport from "passport";
+
+// // Route imports
+// import sheetRoute from "./routes/sheetRoute.js";
+// import authRoute from "./routes/authRoute.js";
+// import geminiRoute from "./routes/geminiRoute.js";
+
+// // Passport config
+// import "./config/passport.js";
+
+// dotenv.config({ path: "./config/config.env" });
+
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+
+// app.use(cors());
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRETE,
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// // Routes
+// app.use("/api/v1", sheetRoute);
+// app.use("/api/v1/auth", authRoute);
+// app.use("/api/v1/gemini", geminiRoute);
+
+// app.get("/", (req, res) => {
+//   res.send(
+//     `Server is working on PORT ${PORT} and project name is Linkedin-Gemini-GoogleSheet-Integration`
+//   );
+// });
+
+// export default app;
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import session from "express-session";
 import passport from "passport";
+import helmet from "helmet";
 
 // Route imports
 import sheetRoute from "./routes/sheetRoute.js";
@@ -31,6 +79,17 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Setting CSP headers to allow fonts
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      fontSrc: ["'self'", "https://linkedin-gemini-gs-server.onrender.com"],
+      // Add other sources as needed
+    },
+  })
+);
 
 // Routes
 app.use("/api/v1", sheetRoute);
--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+// import dotenv from "dotenv";
+// import connectDatabase from "./config/database.js";
+// import app from "./app.js";
+
+// dotenv.config({ path: "./config/config.env" });
+
+// connectDatabase();
+
+// const PORT = process.env.PORT || 4000;
+
+// const server = app.listen(PORT, () => {
+//   console.log(`Server is working on PORT ${PORT}`);
+// });
+
 import dotenv from "dotenv";
 import connectDatabase from "./config/database.js";
 import app from "./app.js";
@@ -10,4 +24,12 @@ const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is working on PORT ${PORT}`);
+});
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self' https://linkedin-gemini-gs-server.onrender.com"
+  );
+  next();
 });
