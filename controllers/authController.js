7a47d2f72c52fc0ef4658c18ec481c823aade5f3
Sakshi kalyan jadhav <sakshikalyanjadhav@Sakshis-MacBook-Air.local>
2024-07-19 15:23:35 +0530

    new commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,57 +1,3 @@
-// // controllers/authController.js
-
-// import passport from "passport";
-
-// //Output: Redirects the user to Google's OAuth page.
-// // Purpose: Initiates the Google OAuth authentication process.
-// // Behavior: When a user attempts to log in with Google, this function redirects them to Google's authentication page. It requests access to the user's Google profile, email, and Google Sheets.
-// export const googleAuth = passport.authenticate("google", {
-//   scope: ["profile", "email", "https://www.googleapis.com/auth/spreadsheets"],
-// });
-
-// // Purpose: Handles the callback from Google after the user has authenticated.
-// // Behavior:
-// // 1. Tries to authenticate the user using Passport's Google strategy.
-// // 2. If authentication fails or there is an error, it redirects the user to the home page ("/").
-// // 3. If successful, it logs the user in and redirects them to the dashboard ("/dashboard").
-// // Output: Redirects the user to the home page on failure or the dashboard on success.
-// export const googleAuthCallback = (req, res) => {
-//   try {
-//     passport.authenticate("google", { failureRedirect: "/" }, (err, user) => {
-//       if (err || !user) {
-//         return res.redirect("/");
-//       }
-
-//       req.logIn(user, (err) => {
-//         if (err) {
-//           return res.redirect("/");
-//         }
-//         // res.redirect("http://localhost:5173/dashboard");
-//         res
-//           .status(200)
-//           .json({ currentUser: user, message: "user login successfully" });
-//       });
-//     })(req, res);
-//   } catch (error) {
-//     res.status(404).json(error);
-//   }
-// };
-
-// export const logout = (req, res, next) => {
-//   try {
-//     req.logout((err) => {
-//       if (err) {
-//         return next(err);
-//       }
-//       res.redirect("/");
-//     });
-//   } catch (error) {
-//     res.status(404).json(error);
-//   }
-// };
-
-// controllers/authController.js
-
 import passport from "passport";
 
 export const googleAuth = passport.authenticate("google", {
@@ -71,7 +17,7 @@ export const googleAuthCallback = (req, res) => {
       // Instead of sending a JSON response, redirect to the frontend with a query string containing user data
       const userData = encodeURIComponent(JSON.stringify(user));
       res.redirect(
-        `https://linkedin-gemini-gs-client.vercel.app/auth/google/callback?user=${userData}`
+        `http://localhost:5173/auth/google/callback?user=${userData}`
       );
     });
   })(req, res);
@@ -85,5 +31,3 @@ export const logout = (req, res, next) => {
     res.redirect("/");
   });
 };
-
-export const sample = (req, res) => {};
